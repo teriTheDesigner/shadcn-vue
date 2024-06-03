fa30453cf7bdc8f2ed365cf38a8c47602b2e1303
teriTheDesigner <tere1109@stud.kea.dk>
2024-06-03 16:35:18 +0200

    routing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Drag from './views/Drag.vue'
+import Home from './views/Home.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  {
+    path: '/drag-and-drop',
+    name: 'DragAndDrop',
+    component: Drag
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/Drag.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Drag and Drop page</h1>
+</template>
+<script>
+export default {
+  name: 'DragAndDrop'
+}
+</script>
